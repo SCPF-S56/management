beda97ff979dbe71537837d1c9d14bed27fc2623
ShhhhhhhhhhhBro <68734556+ShhhhhhhhhhhBro@users.noreply.github.com>
2026-05-20 12:26:21 +0800

    akekwkdk2kw02

--- modules/department_template/main.tf
+++ modules/department_template/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
 resource "github_team" "approvers" {
   name        = "${var.department_name}-approvers"
   privacy     = "closed"
